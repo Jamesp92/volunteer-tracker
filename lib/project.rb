7b0a40c32235201af2af70b29a3aaf47eeeb6590
James Perry <PjPerry28@gmail.com>
2022-10-08 20:12:42 -0700

    pass test for == operator

--- lib/project.rb
+++ lib/project.rb
@@ -6,4 +6,9 @@ class Project
     @title = attributes.fetch(:title)
     @id = attributes.fetch(:id)
   end
+
+  def ==(project_to_compare)
+    self.title() == project_to_compare.title()
+  end
+
 end
